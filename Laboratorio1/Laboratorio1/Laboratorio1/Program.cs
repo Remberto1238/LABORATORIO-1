16268f9923555b91dcde21891ff680f95a1d0239
Remberto1238 <125625467+Remberto1238@users.noreply.github.com>
2023-03-01 20:04:47 -0600

    Add files via upload

---
+++ Laboratorio1/Laboratorio1/Laboratorio1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Laboratorio1.Entidades;
+using Laboratorio1.Negocio;
+
+
+
+ClsPersona Clspersona = new ClsPersona();
+Persona persona = new Persona();
+
+Console.WriteLine("===============================================");
+Console.WriteLine("Ingrese su Nombre: ");
+persona.nombre = (Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingrese su edad: ");
+persona.edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingrese su Sexo: ");
+persona.sexo = (Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingrese su Peso: ");
+persona.peso = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingrese su Altura: ");
+persona.altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(" ");
+Console.WriteLine("===============================================");
+
+Clspersona.PesodePersonas(persona);
+
+foreach (var mostrar in Clspersona.ListadoDePeso())
+{
+    
+}
+
+Console.WriteLine(" ");
+
+Clspersona.MayordeEdad(persona);
+
+foreach (var mostrar in Clspersona.ListadoDeEdad())
+{
+ 
+}
+
+
+Console.ReadKey();
+
